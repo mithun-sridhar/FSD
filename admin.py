54d4b01441c71318fd17b7473d03a139a110e396
Laila Sevim <68811391+200021laila@users.noreply.github.com>
2024-04-30 18:02:05 +1000

    added temproary student controller class

--- admin.py
+++ admin.py
@@ -6,7 +6,11 @@ class Admin:
         self.admin_password: " "
 
     def login_admin():
-
+        # if A for admin is chosen on the University System, enter into admin menu interface
+        if input() == "A":
+            print("Welcome to the Admin System Interface")
+            print("Please select from the following options:")
+        
     
     def view_students():
         ...
