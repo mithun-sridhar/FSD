d90821ab0f69af60b5bed680e246909f87b2c641
Laila Sevim <68811391+200021laila@users.noreply.github.com>
2024-04-30 17:48:25 +1000

    adding function names

--- admin.py
+++ admin.py
@@ -1,3 +1,29 @@
 class Admin:
 
+    def __init__(self):
+        self.admin_id: " "
+        self.admin_name: " "
+        self.admin_password: " "
+
+    def login_admin():
+
     
+    def view_students():
+        ...
+    
+    def delete_student_file():
+        ....
+    
+    def view_descending_grades():
+        ...
+
+    def deregister_student():
+        ...
+
+    def partition_pass_fail():
+        ...
+    
+
+    def remove_student():
+        ...
+
